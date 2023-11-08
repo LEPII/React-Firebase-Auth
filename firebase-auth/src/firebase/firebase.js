b0da04d5043860fe021b25d6fd05a1cefdd2b75c
Luis Perez <lperezdev843@gmail.com>
2023-11-08 16:43:00 -0500

    firebase set up

---
+++ firebase-auth/src/firebase/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD0F9Q0ueLoy1WP9sXmK3GKxSJHFnSqR50",
+    authDomain: "fir-auth-c77e8.firebaseapp.com",
+    projectId: "fir-auth-c77e8",
+    storageBucket: "fir-auth-c77e8.appspot.com",
+    messagingSenderId: "587579304442",
+    appId: "1:587579304442:web:67ba719f48310d7aab5c43",
+    measurementId: "G-ZBZKRRGX4Y"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
